8b2ff43652e0354759988517b29c86301449032f
Kaggenator <47029276+Kaggenator@users.noreply.github.com>
2019-04-28 12:27:11 +0200

    Add files via upload
    Fixat hur banan och kuben ska se ut samt lagt till lite rörelse på kuben.

---
+++ Assets/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        rb.AddForce(0, 0, 500 * Time.deltaTime);
+    }
+}
