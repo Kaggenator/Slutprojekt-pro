349cb956abfc7eab3303a3da2c499755a50cf98b
Kaggenator <47029276+Kaggenator@users.noreply.github.com>
2019-05-07 17:42:51 +0200

    Add files via upload
    Lagt till Obstacles samt börjat fixa koden för kollision

---
+++ Collision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour{
+
+    void OnCollisionEnter(UnityEngine.Collision other)
+    {
+        if(other.collider.tag == "Finish")
+        {
+            
+        }
+    }
+
+}
---
+++ Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public Rigidbody player;
+    public float Xspeed;
+    public float Zspeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        Xspeed = 18f;
+        Zspeed = 10f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Xspeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0, Zspeed * Time.deltaTime);
+    }
+}
